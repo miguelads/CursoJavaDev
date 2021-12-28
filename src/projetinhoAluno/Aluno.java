4d3d603f7309f429232171eeab0e34cc7e6cb364
miguelads <miguelanalista7@gmail.com>
2021-12-28 13:32:59 -0300

    subindo o projeto manualmente da branch master para a main

---
+++ src/projetinhoAluno/Aluno.java
@@ -0,0 +1,126 @@
+package projetinhoAluno;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+	private String nome;
+	private String cpf;
+	private String instituicao;
+	private int anoNascimento;
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	
+	
+	
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getInstituicao() {
+		return instituicao;
+	}
+	public void setInstituicao(String instituicao) {
+		this.instituicao = instituicao;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", cpf=" + cpf + ", instituicao=" + instituicao + ", anoNascimento="
+				+ anoNascimento + ", disciplinas=" + disciplinas + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + anoNascimento;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((disciplinas == null) ? 0 : disciplinas.hashCode());
+		result = prime * result + ((instituicao == null) ? 0 : instituicao.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (anoNascimento != other.anoNascimento)
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (disciplinas == null) {
+			if (other.disciplinas != null)
+				return false;
+		} else if (!disciplinas.equals(other.disciplinas))
+			return false;
+		if (instituicao == null) {
+			if (other.instituicao != null)
+				return false;
+		} else if (!instituicao.equals(other.instituicao))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+	public double calculoMedia() {
+		double media = 0;
+		
+		for(Disciplina disciplina : disciplinas) {
+			media += disciplina.getNota();
+		}
+		
+		return media / disciplinas.size();// ele retorna a quantidade de objetos na lista.
+	}
+	
+	
+	public boolean resultadoAluno() {
+		if(this.calculoMedia() >= 7) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public int calcularIdade() {
+		int ano = Year.now().getValue(); // metodo para pegar o ano atual
+		int resultado = ano - anoNascimento;
+		return resultado;
+	}
+	
+	
+}
---
+++ src/projetinhoAluno/Execucao.java
@@ -0,0 +1,90 @@
+package projetinhoAluno;
+
+import javax.swing.JOptionPane;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*
+	 Disciplina disciplina1 = new Disciplina();
+	 Disciplina disciplina2 = new Disciplina();
+	 Disciplina disciplina3 = new Disciplina();
+	 Disciplina disciplina4 = new Disciplina();
+	 
+	 Aluno aluno = new Aluno();
+	 
+	 disciplina1.setDisciplina("Java Web");
+	 disciplina1.setNota(7.5);
+	 disciplina2.setDisciplina("Java Script");
+	 disciplina2.setNota(8.5);
+	 disciplina3.setDisciplina("HTML & CSS");
+	 disciplina3.setNota(7.5);
+	 disciplina4.setDisciplina("SQL Server");
+	 disciplina4.setNota(9.5);
+	 
+	 aluno.setNome("Junior Miguel");
+	 aluno.setInstituicao("Unibratec");
+	 aluno.setCpf("012.535.924-53");
+	 aluno.getDisciplinas().add(disciplina1);
+	 aluno.getDisciplinas().add(disciplina2);
+	 aluno.getDisciplinas().add(disciplina3);
+	 aluno.getDisciplinas().add(disciplina4);
+	 
+	 System.out.println(aluno.toString());
+	 System.out.println("A média do Aluno foi " + aluno.calculoMedia());
+	 System.out.println("O aluno está " + (aluno.resultadoAluno() ? "aprovado" : "reprovado"));
+	 
+	*/
+		
+		// Fazendo de forma mais dinamica...
+		
+		String nome = JOptionPane.showInputDialog("Nome do Aluno");
+		String cpf = JOptionPane.showInputDialog("CPF do Aluno");
+		String instituicao = JOptionPane.showInputDialog("Instituição do Aluno");
+		String ano = JOptionPane.showInputDialog("Digite o ano de aniversario");
+		
+		// instanciar o aluno para poder inserir os dados
+		Aluno aluno = new Aluno();
+		aluno.setNome(nome);
+		aluno.setCpf(cpf);
+		aluno.setInstituicao(instituicao);
+		aluno.setAnoNascimento(Integer.valueOf(ano));
+		
+		// deixando de forma dinamica construindo um for para colocar 4 notas e 4 disciplinas
+		
+		for(int pos =1; pos <= 4; pos++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos);
+			String notaDisciplina = JOptionPane.showInputDialog("Nota " + pos);
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));// double.ValueOf() convert de String para double
+			
+			aluno.getDisciplinas().add(disciplina);
+		}
+		
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+		if(escolha == 0) {
+		  /*no array ele remove por inteiros e no remove no metodo ele remove por objetos
+		   * então usamos o método intValue() para remover pelo indice da lista e o -1 para 
+		   * pegar o valor exato ja que em toda lista se inicia com o zero!*/
+		  int continuarRemover = 0;
+		  int pos = 1;
+		  while(continuarRemover == 0) {
+			  String removerDisciplna = JOptionPane.showInputDialog("Qual a disciplina 1, 2, 3 ou 4");
+			  aluno.getDisciplinas().remove(Integer.valueOf(removerDisciplna).intValue() -pos); 
+			  pos++; // este incremento serve para tirar um erro de excluir o 2 e depois o 4 e não da erro de posição
+			  continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a Remover");
+			  System.out.println(aluno.toString());
+		  }
+		}
+		
+		System.out.println(aluno.toString());
+		System.out.println("A média do aluno é " + aluno.calculoMedia());
+		System.out.println("O aluno está " + (aluno.resultadoAluno() ? " aprovado " : " reprovado "));
+		System.out.println("Sua idade é " + aluno.calcularIdade());
+	}
+
+}
